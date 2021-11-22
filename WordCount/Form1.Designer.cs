20e8e83b0e9a6e19108fc256bf3a63f0dfae6414
SerjPi <monsup@mail.ru>
2021-11-22 16:51:18 +0300

    Первая версия. Сделал разбор текстового файла на слова. Подсчет вхождений.

--- WordCount/Form1.Designer.cs
+++ WordCount/Form1.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent( )
         {
-            this.components = new System.ComponentModel.Container( );
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpenDir = new System.Windows.Forms.Button();
+            this.lbListFiles = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbOrdeByQuantity = new System.Windows.Forms.RadioButton();
+            this.rbOrdeByName = new System.Windows.Forms.RadioButton();
+            this.lbWords = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOpenDir);
+            this.groupBox1.Controls.Add(this.lbListFiles);
+            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 275);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список текстовых файлов";
+            // 
+            // btnOpenDir
+            // 
+            this.btnOpenDir.Location = new System.Drawing.Point(6, 30);
+            this.btnOpenDir.Name = "btnOpenDir";
+            this.btnOpenDir.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenDir.TabIndex = 1;
+            this.btnOpenDir.Text = "Папка";
+            this.btnOpenDir.UseVisualStyleBackColor = true;
+            this.btnOpenDir.Click += new System.EventHandler(this.btnOpenDir_Click);
+            // 
+            // lbListFiles
+            // 
+            this.lbListFiles.FormattingEnabled = true;
+            this.lbListFiles.Location = new System.Drawing.Point(6, 68);
+            this.lbListFiles.Name = "lbListFiles";
+            this.lbListFiles.Size = new System.Drawing.Size(209, 199);
+            this.lbListFiles.TabIndex = 0;
+            this.lbListFiles.DoubleClick += new System.EventHandler(this.lbListFiles_DoubleClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.rbOrdeByQuantity);
+            this.groupBox2.Controls.Add(this.rbOrdeByName);
+            this.groupBox2.Controls.Add(this.lbWords);
+            this.groupBox2.Location = new System.Drawing.Point(277, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(285, 426);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Список слов";
+            // 
+            // rbOrdeByQuantity
+            // 
+            this.rbOrdeByQuantity.AutoSize = true;
+            this.rbOrdeByQuantity.Location = new System.Drawing.Point(6, 400);
+            this.rbOrdeByQuantity.Name = "rbOrdeByQuantity";
+            this.rbOrdeByQuantity.Size = new System.Drawing.Size(99, 17);
+            this.rbOrdeByQuantity.TabIndex = 2;
+            this.rbOrdeByQuantity.TabStop = true;
+            this.rbOrdeByQuantity.Text = "По количеству";
+            this.rbOrdeByQuantity.UseVisualStyleBackColor = true;
+            this.rbOrdeByQuantity.CheckedChanged += new System.EventHandler(this.rbOrdeByQuantity_CheckedChanged);
+            // 
+            // rbOrdeByName
+            // 
+            this.rbOrdeByName.AutoSize = true;
+            this.rbOrdeByName.Location = new System.Drawing.Point(6, 377);
+            this.rbOrdeByName.Name = "rbOrdeByName";
+            this.rbOrdeByName.Size = new System.Drawing.Size(90, 17);
+            this.rbOrdeByName.TabIndex = 1;
+            this.rbOrdeByName.TabStop = true;
+            this.rbOrdeByName.Text = "По алфавиту";
+            this.rbOrdeByName.UseVisualStyleBackColor = true;
+            // 
+            // lbWords
+            // 
+            this.lbWords.FormattingEnabled = true;
+            this.lbWords.Location = new System.Drawing.Point(6, 16);
+            this.lbWords.Name = "lbWords";
+            this.lbWords.Size = new System.Drawing.Size(273, 355);
+            this.lbWords.TabIndex = 0;
+            this.lbWords.DoubleClick += new System.EventHandler(this.lbWords_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество слов: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size( 800, 450 );
+            this.ClientSize = new System.Drawing.Size(580, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpenDir;
+        private System.Windows.Forms.ListBox lbListFiles;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbOrdeByQuantity;
+        private System.Windows.Forms.RadioButton rbOrdeByName;
+        private System.Windows.Forms.ListBox lbWords;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WordCount/Form1.cs
+++ WordCount/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,121 @@ namespace WordCount
 {
     public partial class Form1 : Form
     {
+        private List<FileInfo> ListFiles = new List<FileInfo>( );
+        private List<String> ListWorlds = new List<string>( );
         public Form1( )
         {
             InitializeComponent( );
+        }
+
+        private void btnOpenDir_Click( object sender, EventArgs e )
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lbListFiles.Items.Clear( );
+                ListFiles.Clear( );
+                DirectoryInfo rootDir = Directory.CreateDirectory( folderBrowserDialog1.SelectedPath );
+                rootDir.Create( );
+                foreach (FileInfo item in rootDir.GetFiles( "*.txt", SearchOption.TopDirectoryOnly ))
+                {
+                    ListFiles.Add( item );
+                    lbListFiles.Items.Add( item.Name );
+                }
+                rbOrdeByQuantity.Checked = true;
+
+            }
+            
+        }
+
+        //Обработчик двойного щелчка по выбранному текстовому файлу
+        private void lbListFiles_DoubleClick( object sender, EventArgs e )
+        {
+            
+            if (lbListFiles.SelectedItem != null)
+            {
+                FileToWords( ListFiles[ lbListFiles.SelectedIndex ].FullName, ListWorlds );
+                //CountWords( ListWorlds , rbOrdeByQuantity.Checked);
+                ViewListStrings( new List<string>( CountWords( ListWorlds, rbOrdeByQuantity.Checked )), lbWords );
+                label1.Text = $"Количество слов: {CountWords( ListWorlds, rbOrdeByQuantity.Checked ).Count( )}";
+            }
+        }
+
+        private IEnumerable<string> CountWords( List<string> listWorlds, bool sortQuantity )
+        {
+            List<string> resultList = new List<string>( );
+            var res = ListWorlds.Where( s => s.Length > 2 )
+                   .GroupBy( s => s )
+                   .Select( s => new
+                   {
+                       word = s.Key,
+                       count = s.Count( )
+                   } );
+            if (sortQuantity)
+            {
+                foreach (var item in res.OrderByDescending(s => s.count))
+                {
+                    resultList.Add( $"{item.word} - {item.count}" );
+                }
+                return resultList;
+            }
+            foreach (var item in res.OrderBy( s => s.word ))
+            {
+                resultList.Add( $"{item.word} - {item.count}" );
+            }
+            return resultList;
+
+        }
+
+        private void ViewListStrings( List<string> listWorlds, ListBox _lbWords )
+        {
+            _lbWords.Items.Clear( );
+            _lbWords.Items.AddRange( listWorlds.ToArray<string>( ) );
+        }
+
+        //Из файла в список слов
+        private void FileToWords( string fullName , List<string> words)
+        {
+            words.Clear( );
+            // Символы разделители
+            //char[] separator =new char[] { '.', ' ', ',', ':', ';', '%','-','"', '\u2013','»', '«'
+            //    ,'[',']','<','>','\r','\n',
+            //                                ')','(', '\u0021', '\u0160', '\u0044',
+            //                                '\u0013','?','!','&', '\u2026','\u0010'};
+
+            char[] separator = new char[] { '\r','\n',' ','.',',','[',']','!','?',':',';','\u2014','\u2013'
+                                            ,'\u00AB', '\u00BB'};
+
+            //Считывает файл и разбиваем на слова
+            string temp = File.ReadAllText( fullName, Encoding.GetEncoding( "windows-1251" ) );
+            string[] txtToStringArray = File.ReadAllText( fullName, Encoding.GetEncoding( "windows-1251" ) )
+                .Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            //
+            foreach (var item in txtToStringArray)
+            {
+                words.Add( item.ToLowerInvariant().Trim( ) );
+            }
+           
+        }
+
+      
+        private void lbWords_DoubleClick( object sender, EventArgs e )
+        {
+            if (lbWords.SelectedItem != null)
+            {
+                string resultChars = "";
+                foreach (var item in (string)lbWords.Items[ lbWords.SelectedIndex ])
+                {
+                    resultChars +=  $"{(int)item}\t";
+                }
+                MessageBox.Show( resultChars );
+            }
+
+        }
+
+        private void rbOrdeByQuantity_CheckedChanged( object sender, EventArgs e )
+        {
+            ViewListStrings( new List<string>( CountWords( ListWorlds, rbOrdeByQuantity.Checked ) ), lbWords );
         }
     }
 }
